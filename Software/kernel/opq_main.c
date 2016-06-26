f7694b32d6d48f1d5b8e9fe27cd5061f9406be40
Serge Negrashov <sergey@openpowerquality.org>
2016-06-26 18:11:41 +0000

    Driver is finished

--- Software/kernel/opq_main.c
+++ Software/kernel/opq_main.c
@@ -85,6 +85,7 @@ static irqreturn_t opq_gpio_irq_handler(int irq, void *dev_id, struct pt_regs *r
 
 //This will set up the irq
 int opq_init_gpio_int(void) {
+
     if (gpio_request(OPQ_DATA_READY_GPIO, OPQ_DATA_READY_GPIO_DESC)) {
         printk("OPQ: GPIO request faiure: %s\n", OPQ_DATA_READY_GPIO_DESC);
         return -ENODEV;
@@ -113,7 +114,7 @@ int opq_init_gpio_int(void) {
 
 //This will release the IRQ
 void opq_release_gpio_int(void) {
-   // free_irq(opq_dev->irq_any_gpio, OPQ_DATA_READY_GPIO_DEVICE_DESC);
+    free_irq(opq_dev->irq_any_gpio, OPQ_DATA_READY_GPIO_DEVICE_DESC);
     gpio_free(OPQ_DATA_READY_GPIO);
     cancel_work_sync(&opq_wq_spi_reader);
     destroy_workqueue(opq_dev->wq);
@@ -128,7 +129,7 @@ int opq_init_spi(void) {
         .max_speed_hz = OPQ_SPI_MAX_SPEED,
         .bus_num = OPQ_SPI_BUS,
         .chip_select = OPQ_SPI_CHAN,
-        .mode = 0,
+        .mode = 0x0,
     };
     int ret;
     struct spi_master *master;
@@ -157,20 +158,31 @@ void opq_release_spi(void) {
 static void opq_wq_spi_reader_handler(struct work_struct *w) {
     mutex_lock(opq_dev->cycles->mutex);
     spi_read(opq_dev->spi_device, opq_dev->cycles->data[opq_dev->cycles->writer_pos], OPQ_CYCLE_BUFFER_SIZE);
-
     opq_dev->cycles->writer_pos++;
     if(opq_dev->cycles->writer_pos >= OPQ_CYCLE_BUFFER_COUNT)
         opq_dev->cycles->writer_pos  = 0;
+    if(opq_dev->cycles->writer_pos  == opq_dev->cycles->reader_pos){
+        //Reader is not keeping up. Have to evict an item.
+        //Note we are not touching the semaphore in this case
+        //since number of items has not changed.
+        opq_dev->cycles->reader_pos++;
+        if(opq_dev->cycles->reader_pos >= OPQ_CYCLE_BUFFER_COUNT)
+            opq_dev->cycles->reader_pos  = 0;
+
+    }
+    else{
+        up(opq_dev->cycles->sem);
+    }
     mutex_unlock(opq_dev->cycles->mutex);
     //printk("hello\n");
 }
 //Chardev
 
-int opq_chardev_major=-1;
+static int opq_chardev_major=-1;
+static struct file_operations opq_chardev_fops;
 
 int opq_init_chardev()
 {
-    struct file_operations opq_chardev_fops;
     dev_t tmpchrdev;
 
     opq_chardev_fops.open =  opq_chardev_open;
@@ -180,7 +192,7 @@ int opq_init_chardev()
     opq_chardev_fops.llseek = opq_chardev_lseek;
     if(alloc_chrdev_region (&tmpchrdev,0,1,"opq_chardev")<0)
     {
-        printk("opq driver: couldnt get major/minor number.\n");
+        printk("OPQ: couldnt get major/minor number.\n");
         return -1;
     }
     opq_chardev_major=MAJOR(tmpchrdev);
@@ -189,10 +201,10 @@ int opq_init_chardev()
     opq_dev->cdev.ops = &opq_chardev_fops;
     if(cdev_add(&opq_dev->cdev, MKDEV(opq_chardev_major, 0), 1) < 0)
     {
-        printk("opq driver: Failed to initialize character device\n");
+        printk("OPQ: Failed to initialize character device\n");
         return -1;
     }
-    printk("opq driver: Initialized character device with major number %d\n", opq_chardev_major);
+    printk("OPQ: Initialized character device with major number %d\n", opq_chardev_major);
     return 0;
 }
 
@@ -204,6 +216,7 @@ void opq_release_chardev(void)
 
 int opq_chardev_open(struct inode *inode,struct file *filep)
 {
+    printk(KERN_INFO "OPQ: %d opened the pci driver' chr dev\n", current->pid);
     return 0;
 }
 
@@ -216,15 +229,20 @@ int opq_chardev_release(struct inode *inode,struct file *filep)
 
 ssize_t opq_chardev_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos)
 {
+    if (down_interruptible(opq_dev->cycles->sem))
+        return -ERESTARTSYS;
     mutex_lock(opq_dev->cycles->mutex);
     if(count > OPQ_CYCLE_BUFFER_SIZE)
         count = OPQ_CYCLE_BUFFER_SIZE;
 
     //Read SPI here
-    if(copy_to_user(buf, opq_dev->cycles->data[0], count) != 0)
+    if(copy_to_user(buf, opq_dev->cycles->data[opq_dev->cycles->reader_pos], count) != 0)
     {
         printk("KERN_ERROR" "OPQ: copy to user failed\n");
     }
+    opq_dev->cycles->reader_pos++;
+    if(opq_dev->cycles->reader_pos >= OPQ_CYCLE_BUFFER_COUNT)
+        opq_dev->cycles->reader_pos = 0;
     mutex_unlock(opq_dev->cycles->mutex);
     return count;
 }
@@ -232,12 +250,12 @@ ssize_t opq_chardev_read(struct file *filp, char __user *buf, size_t count, loff
 
 ssize_t opq_chardev_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos)
 {
-    return -EINVAL;
+    return 0;
 }
 
 loff_t opq_chardev_lseek(struct file *file, loff_t offset, int orig)
 {
-    return -EINVAL;
+    return 0;
 }
 
 //Circular buffer stuff
@@ -247,7 +265,7 @@ void opq_circ_buffer_init()
     opq_dev->cycles->mutex = kmalloc(sizeof(struct mutex), GFP_KERNEL);
     opq_dev->cycles->sem = kmalloc(sizeof(struct semaphore), GFP_KERNEL);
     mutex_init(opq_dev->cycles->mutex);
- //   sema_init(opq_dev->cycles->sem, 0);
+    sema_init(opq_dev->cycles->sem, 0);
     opq_dev->cycles->writer_pos = 1;
     opq_dev->cycles->reader_pos = 0;
 }
@@ -264,7 +282,7 @@ static int __init opq_module_init(void)
     opq_dev = kmalloc(sizeof(struct opq_device), GFP_KERNEL);
     opq_dev->int_cnt = 0;
     opq_circ_buffer_init();
-    //opq_init_chardev();
+    opq_init_chardev();
     opq_init_spi();
     opq_init_gpio_int();
     printk (KERN_NOTICE "OPQ: OPQ driver loaded!\n");
@@ -273,7 +291,7 @@ static int __init opq_module_init(void)
 
 static void __exit opq_module_exit(void)
 {
-    //opq_release_chardev();
+    opq_release_chardev();
     opq_release_spi();
     opq_release_gpio_int();
     opq_circ_buffer_remove();
